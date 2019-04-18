a6342a877d57df160433dc1c8f32d3277b3e4736
kernelland <kernelland@protonmail.ch>
2019-04-18 16:13:12 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,8 +5,8 @@ def predict_loop_time(counter,loop_max,start_time,nums_behind_zero=2):
         current_time = time()
         elapsed_time = current_time - start_time
         time_left = loop_max * elapsed_time / counter - elapsed_time
-        minst_left = float(str([int(time_left)/100 if int(time_left / 60) < 1 else (time_left)/60][0])[:int(2+nums_behind_zero)])
-        return minst_left
+        mins_left = float(str([int(time_left)/100 if int(time_left / 60) < 1 else (time_left)/60][0])[:int(2+nums_behind_zero)])
+        return mins_left
     except ZeroDivisionError:
         return 99999999999
 
